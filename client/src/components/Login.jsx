999058b4628a76fc12d46f4200d78d9d8539c2ce
sumandashw16 <sumandash2004@gmail.com>
2026-03-13 20:15:48 +0530

    changes made to login.jsx, it has endpoint on register

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -13,7 +13,7 @@ export default function Login() {
     setError("");
 
     try {
-      const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/register`, {
+      const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
